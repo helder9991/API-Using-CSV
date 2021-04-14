fc996be3a361f2d1e1faa383bb25377e9132f9bf
Helder Afonso <helderafonsoss@hotmail.com>
2021-04-14 00:00:00 -0300

    Criaçao da funcionalidade de listagem de usuarios

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import { CreateUserCSVController } from '@modules/users/useCases/CreateUserCSVController';
+import { ListUsersController } from '@modules/users/useCases/ListUsersController';
 
 const usersRoutes = Router();
 
 const createUserCSVController = new CreateUserCSVController();
+const listUsersController = new ListUsersController();
 
 const upload = multer({
     dest: './tmp',
@@ -16,5 +18,7 @@ usersRoutes.post(
     upload.single('file'),
     createUserCSVController.handle
 );
+
+usersRoutes.get('/list', listUsersController.handle);
 
 export { usersRoutes };
